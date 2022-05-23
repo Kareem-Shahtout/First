633fb341747aae131eab79e87e9a251f3e8cd97e
Kareem-Shahtout <abdfathisha89@gmail.com>
2022-05-24 02:30:59 +0300

    Nothing

---
+++ routes/web.php
@@ -0,0 +1,18 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('myname', function () {
+    return ('<h1>welcome Abd Al-kareem to php</h1><br><h2>1301198619<h2>');
+});
