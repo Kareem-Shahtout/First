fd3f1186ae4460ca90a53eb06e5d5b00f5cb59f5
Kareem-Shahtout <abdfathisha89@gmail.com>
2022-05-26 21:20:15 +0300

    Routing

--- routes/web.php
+++ routes/web.php
@@ -12,6 +12,24 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::get('user/{number}/age/{age}',function($number,$age){
+    return 'My number is :' . '<b>' . $number .'</b>' . ', And my age is : '. $age;
+});
+
+Route::get('Any',function(){
+    return redirect()->route('information');
+});
+Route::view('Hello','kareemblade',['name' => 'Abd Alkareem']);
+
+Route::redirect('facebook','http://www.facebook.com',301);
+
+Route::redirect('old','login');
+
+Route::get('age',function(){
+    return 'My age is 21';
+})->name('information');
+
+Route::get('login' , 'App\Http\Controllers\Controller@login');
 
 Route::get('myname', function () {
     return ('<h1>welcome Abd Al-kareem to php</h1><br><h2>1301198619<h2>');
